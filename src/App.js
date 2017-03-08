79826d2abc25218b41744e3b7c8b69014e6dfc74
chelle tuerk <michelletuerk@gmail.com>
2017-03-08 09:48:23 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>HIGH PLAINS TRENCHING</h2>
+        </div>
+        <p className="App-intro">
+        </p>
+        <div className='footer'>
+          <h4>Michael L. Tuerk</h4>
+          <h4>89454 Turkey Creek Ave. Springview, NE 68778</h4>
+          <h4>(402) 497-3845</h4>
+          <h4><a href='mailto:mltuerk@gmail.com'>mltuerk@gmail.com</a></h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
